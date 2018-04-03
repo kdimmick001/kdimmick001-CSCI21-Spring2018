8f39a2c7ebdab82281b9cf64cb9654815b615337
kdimmick001 <kdimmick001@student.butte.edu>
2018-04-03 17:31:00 +0000

    Nodes

--- Nodes/node.h
+++ Nodes/node.h
@@ -8,13 +8,31 @@ using namespace std;
 #ifndef NODE_H
 #define NODE_H
 
+template<typename DT>
 class Node {
     private:
-        string thing;
-        Node* next; //Points to the next element of the 
+        DT thing_;
+        Node *next; //Points to the next object from the Node class.
+        Node *previous; //Points to the previous object from the Node class.
+        Node *last; //Points to the last object.
+        Node *first; // Points to the first object.
         
     public:
+        //Constructor to set the pointer.
         Node();
+        //To set the information being held in the object.
+        void set_contents(DT info);
+        //To get the information being held in the object.
+        DT contents();
+        //To set the next node pointer to the next spot. As well as the previous to the previous spot.
+        void set_next_node(Node *node2);
+        //Returns the next node object pointer.
+        Node get_next_node();
+        //Sets the previous node to the last node object.
+        void set_previous_node(Node *node2);
+        //Gets the previous node object.
+        Node get_previous_node();
+        
 };
 
 #endif
--- Nodes/node_class.cpp
+++ Nodes/node_class.cpp
@@ -1,15 +1,54 @@
-#include <fstream>
-#include <iostream>
-#include <string>
-#include <sstream>
-#include <cstring>
-using namespace std;
-
 #include "node.h"
 
-Node::Node(string in_thing = "none", Node* next_location = NULL){
-	this->thing = in_thing;
-	this->next_thing = next_location;
-	return;
+//Move this to .cpp file.
+//Constructor.
+Node::Node(){
+    next = NULL;
+    previous = NULL;
+};
+//To set the thing to the parameter value.
+template<typename DT>
+void Node::set_contents(DT info){
+    thing_ = info;
+    return;
 };
 
+//To get the information being held in the object.
+template<typename DT>
+DT Node::contents(){
+    return thing_;  
+};
+
+//To set t he next pointer.
+void Node::set_next_node(Node *node2){
+    //Sets the next object pointer to the node2 object.
+    next = node2;
+    //Sets the previous object pointer of node2 to this object.
+    node2.set_previous_node(this);
+    //If the next for the node2 object is NULL it will be set to last.
+    if(node2.get_next_node() == NULL){
+        last = node2;
+    }
+    //If the previous pointer is NULL then the object will be first.
+    if (previous == NULL){
+        first = this;
+    }
+    return;
+};
+
+Node Node::get_next_node(){
+    //Returns the object that the next is pointing to.
+    return *next;
+};
+
+void Node::set_previous_node(Node *node2){
+    //To set the previous pointer.
+    previous = node2;
+    return;
+};
+
+Node Node::get_previous_node(){
+    //Returns the object that the previous is pointing to.
+    return *previous;
+};
+    
