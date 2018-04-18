968c80f6ee09cfd9c336b97d86851bd01c902cf6
kdimmick001 <kdimmick001@student.butte.edu>
2018-04-18 00:13:22 +0000

    Week8

---
+++ Week8/movie.cpp
@@ -0,0 +1,28 @@
+string coActors(string name){
+    
+}
+
+int main(){
+    
+    ifstream fin;
+    fin.open("movies.txt");
+    if (!fin.is_open()){
+        cout << "UnBle to open file." << endl;
+    }
+    //To keep track of how long the file is.
+    int count;
+    //Creating a hash table.
+    movie_database = new HashTable
+    while(!fin.eof()){
+        fin >> first_name;
+        fin >> last_name;
+        fin >> movie;
+        //To check if movie has already been entered.
+        while (){
+            
+        }
+        HashInsert(movie)
+        //To keep track of the size.
+        ++count;
+    }
+}
